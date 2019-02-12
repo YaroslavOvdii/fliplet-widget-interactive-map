dce11828df20ad5d19577c39b65ced764a0dbbe9
hcarneiro <hcarneiro@weboo.biz>
2019-02-12 19:26:22 +0000

    To make sure the marker is placed in the middle of where the user clicks

--- js/libs/build.js
+++ js/libs/build.js
@@ -146,7 +146,7 @@ Fliplet.Widget.instance('interactive-map', function(widgetData) {
 
         this.mappedMarkerData.forEach((marker, index) => {
           if (marker.data.map === this.selectedMapData.name) {
-            const markerElem = $("<div id='" + marker.id + "' class='marker' data-name='" + marker.data.name + "' style='left: -15px; top: -15px; position: absolute; width: " + marker.data.size + "; height: " + marker.data.size + "; font-size: " + marker.data.size + ";'><i class='" + marker.data.icon + "' style='color: " + marker.data.color + "; font-size: " + marker.data.size + ";'></i><div class='active-state'><i class='" + marker.data.icon + "' style='color: " + marker.data.color + ";'></i></div></div>")
+            const markerElem = $("<div id='" + marker.id + "' class='marker' data-name='" + marker.data.name + "' style='left: -15px; top: -15px; position: absolute; font-size: " + marker.data.size + ";'><i class='" + marker.data.icon + "' style='color: " + marker.data.color + "; font-size: " + marker.data.size + ";'></i><div class='active-state'><i class='" + marker.data.icon + "' style='color: " + marker.data.color + ";'></i></div></div>")
 
             this.markerElemHandler = new Hammer(markerElem.get(0))
             this.flPanZoomInstance.markers.set([Fliplet.UI.PanZoom.Markers.create(markerElem, { x: marker.data.positionx, y: marker.data.positiony, name: marker.data.name, id: marker.id })])
