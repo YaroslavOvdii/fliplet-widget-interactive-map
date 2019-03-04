0ef893b0fdd3f59c83a8b60b6fe04e8c68a93499
hcarneiro <hcarneiro@weboo.biz>
2019-03-04 10:52:02 +0000

    Typo

--- dist/map-panel.js
+++ dist/map-panel.js
@@ -155,7 +155,7 @@ Fliplet.InteractiveMap.component('map-panel', {
         var imageUrl = result.data[0].url;
         var pattern = /[?&]size=/;
 
-        if (!pattern.test(url)) {
+        if (!pattern.test(imageUrl)) {
           var params = imageUrl.substring(1).split('?');
           imageUrl += (params.length > 1 ? '&' : '?') + 'size=large';
         }
